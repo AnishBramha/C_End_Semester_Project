ce28fc9fc44261417658a0c04555ceec9e810811
Aryan-Sharma05 <aryan.sharma@iiitb.ac.in>
2024-11-16 03:49:43 +0530

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,1 +1,32 @@
 
+#define CLEAR_SCREEN system("cls");
+
+// Constants for menu layout
+#define MAX_ITEM_NAME_WIDTH  21
+#define MAX_ITEM_NO_WIDTH     8
+#define MAX_SPICE_WIDTH      10
+#define MAX_TYPE_WIDTH        11
+#define MAX_ALLERGEN_WIDTH   15
+#define MAX_PRICE_WIDTH       7
+
+// Terminal color codes
+#define BLUE "\033[96m"
+#define RED "\033[91m"
+#define YELLOW "\033[33m"
+#define BLINKING_YELLOW "\033[93m"
+#define RESET "\033[0m"
+
+// Function Prototypes
+void logo(void);
+void StartupPage(void);
+void LoginPage(void);
+void CustomerDetailsPage(char name[], char phone[], int *persons, int flag);
+void ManagerLoginPage(char username[], char password[], int flag);
+void Customerloggedinpage(void);
+void ManagerLoggedinpage(void);
+void menuLogo(void);
+void menuHeader(void);
+void menuFooter(void);
+void menuRow(const char *item_name, const char *item_no, const char *spice, const char *type, const char *allergens, const char *price,int veg);
+int get_wrapped_lines(const char *text, int max_width);
+void print_wrapped_text(const char *text, int max_width, int line);
