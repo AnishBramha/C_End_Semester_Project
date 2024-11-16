61619d592fd034884c8adc951438570327744e4c
AnishBramha <Anish.Teja@iiitb.ac.in>
2024-11-17 00:04:34 +0530

    Updated linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -3,6 +3,7 @@
 #include<stdbool.h>
 #include<stdlib.h>
 #include<string.h>
+#include "file_handler.h"
 
 #define STR_INDEX_ERR fprintf(stderr, "\a\nFATAL ERROR! INVALID INDEX!\n")
 #define STR_MEM_ERR fprintf(stderr, "\a\nFATAL ERROR! ALLOCATION FAILED!\n")
@@ -20,30 +21,22 @@ void* alloc(size_t size) {
     return mem;
 }
 
-typedef struct {
 
-    char itemID[7];
-    char name[100];
-    float price;
-    char allergens[100];
 
-} Menu;
+NodeMenu* __newItem__(Menu*);
+NodeTable* __new__Table(Table*);
+void __delMenu__(NodeMenu*);
+void __delTable__(NodeTable*);
 
-typedef struct Node {
+NodeMenu* pushItem(int, NodeMenu*, NodeMenu*);
+NodeMenu* popItem(int, NodeMenu*);
+NodeTable* pushTable(int, NodeTable*, NodeTable*);
+NodeTable* popTable(int, NodeTable*);
 
-    Menu item;
-    struct Node* next;
-
-} Node;
-
-Node* __new__(Menu*);
-void __del__(Node*);
-
-Node* push(int, Node*, Node*);
-Node* pop(int, Node*);
-
-int len(Node*);
-void __str__(Node*);
+int lenMenu(NodeMenu*);
+int lenTable(NodeTable*);
+void __strMenu__(NodeMenu*);
+void __strTable__(NodeTable*);
 
 
 
@@ -53,15 +46,19 @@ void __str__(Node*);
 
 
 
-void test(void); // use for testing purposes only
-void format(Node*);
 
-int main(void) {
+void testMenu(void); // use for testing purposes only
+void testTable(void); // use for testing purposes only
+void formatItem(NodeMenu*);
+void formatTable(NodeTable*);
 
-    //test();
+// int main(void) {
 
-    return 0;
-}
+//     //testMenu();
+//     //testTable();
+
+//     return 0;
+// }
 
 
 
@@ -74,9 +71,9 @@ int main(void) {
 
 
 
-Node* __new__(Menu* item) {
+NodeMenu* __newItem__(Menu* item) {
 
-    Node* node = (Node*)alloc(sizeof(Node));
+    NodeMenu* node = (NodeMenu*)alloc(sizeof(NodeMenu));
 
     node->item = *item;
     node->next = NULL;
@@ -84,11 +81,19 @@ Node* __new__(Menu* item) {
     return node;
 }
 
-void __del__(Node* head) {
+NodeTable* __newTable__(Table* table) {
+
+    NodeTable* node = (NodeTable*)alloc(sizeof(NodeTable));
+
+    node->table = *table;
+    node->next = NULL;
+}
+
+void __delMenu__(NodeMenu* head) {
 
     while (head) {
 
-        Node* temp = head;
+        NodeMenu* temp = head;
         head = head->next;
         free(temp);
     }
@@ -96,10 +101,10 @@ void __del__(Node* head) {
     return;
 }
 
-int len(Node* head) {
+int lenMenu(NodeMenu* head) {
 
     int size = 0;
-    Node* temp = head;
+    NodeMenu* temp = head;
 
     while (temp) {
         size++;
@@ -109,16 +114,29 @@ int len(Node* head) {
     return size;
 }
 
-void __str__(Node* head) {
+int lenTable(NodeTable* head) {
+
+    int size = 0;
+    NodeTable* temp = head;
+
+    while (temp) {
+        size++;
+        temp = temp->next;
+    }
+
+    return size;
+}
+
+void __strMenu__(NodeMenu* head) {
 
     if (!head) {
         STR_EMPTY_LIST_ERR;
         return;
     }
 
-    Node* temp = head;
+    NodeMenu* temp = head;
 
-    for (int i = 0; i < len(head); i++) {
+    for (int i = 0; i < lenMenu(head); i++) {
 
         printf("Item ID: %s, Name: %s, Price: %.2f, Allergens: %s\n", temp->item.itemID, temp->item.name, temp->item.price, temp->item.allergens);
 
@@ -128,7 +146,26 @@ void __str__(Node* head) {
     return;
 }
 
-Node* push(int index, Node* head, Node* insert) {
+void __strTable__(NodeTable* head) {
+
+    if (!head) {
+        STR_EMPTY_LIST_ERR;
+        return;
+    }
+
+    NodeTable* temp = head;
+
+    for (int i = 0; i < lenTable(head); i++) {
+
+        printf("Number: %d, Capacity: %d, Availability: %d\n", temp->table.number, temp->table.capacity, temp->table.available);
+
+        temp = temp->next;
+    }
+
+    return;
+}
+
+NodeMenu* pushItem(int index, NodeMenu* head, NodeMenu* insert) {
 
     if (!index) {
 
@@ -136,12 +173,12 @@ Node* push(int index, Node* head, Node* insert) {
         return insert;
     }
 
-    if (index >= len(head) || index < 0) {
+    if (index >= lenMenu(head) || index < 0) {
         STR_INDEX_ERR;
         return head;
     }
 
-    Node* temp = head;
+    NodeMenu* temp = head;
     for (int i = 0; i < index; i++)
         temp = temp->next;
 
@@ -151,33 +188,56 @@ Node* push(int index, Node* head, Node* insert) {
     return head;  
 }
 
-Node* pop(int index, Node* head) {
+NodeTable* pushTable(int index, NodeTable* head, NodeTable* insert) {
+
+    if (!index) {
+
+        insert->next = head;
+        return insert;
+    }
+
+    if (index >= lenTable(head) || index < 0) {
+        STR_INDEX_ERR;
+        return head;
+    }
+
+    NodeTable* temp = head;
+    for (int i = 0; i < index; i++)
+        temp = temp->next;
+
+    insert->next = temp->next;
+    temp->next = insert;
+
+    return head;  
+}
+
+NodeMenu* popItem(int index, NodeMenu* head) {
 
     if (!head) {
         STR_EMPTY_LIST_ERR;
         return NULL;
     }
 
-    if (index >= len(head) || index < 0) {
+    if (index >= lenMenu(head) || index < 0) {
         STR_INDEX_ERR;
         return head;
     }
 
     if (!index) {
 
-        Node* temp = head;
+        NodeMenu* temp = head;
         head = head->next;
 
         free(temp);
         return head;
     }
 
-    Node* temp = head;
+    NodeMenu* temp = head;
     
     for (int i = 0; i < index - 1; i++)
         temp = temp->next;
 
-    if (index == len(head) - 1) {
+    if (index == lenMenu(head) - 1) {
 
         free(temp->next);
         temp->next = NULL;
@@ -185,64 +245,131 @@ Node* pop(int index, Node* head) {
         return head;
     }
 
-    Node* node_del = temp->next;
+    NodeMenu* node_del = temp->next;
     temp->next = temp->next->next;
     free(node_del);
 
     return head;
 }
 
-void format(Node* list) {
+NodeTable* popTable(int index, NodeTable* head) {
 
-    puts("---------------------------------");
-    __str__(list);
-    printf("LENGTH: %d\n", len(list));
+    if (!head) {
+        STR_EMPTY_LIST_ERR;
+        return NULL;
+    }
+
+    if (index >= lenTable(head) || index < 0) {
+        STR_INDEX_ERR;
+        return head;
+    }
+
+    if (!index) {
+
+        NodeTable* temp = head;
+        head = head->next;
+
+        free(temp);
+        return head;
+    }
+
+    NodeTable* temp = head;
+    
+    for (int i = 0; i < index - 1; i++)
+        temp = temp->next;
+
+    if (index == lenTable(head) - 1) {
+
+        free(temp->next);
+        temp->next = NULL;
+
+        return head;
+    }
+
+    NodeTable* node_del = temp->next;
+    temp->next = temp->next->next;
+    free(node_del);
+
+    return head;
 }
 
-void test(void) {
+void formatItem(NodeMenu* list) {
+
+    puts("---------------------------------");
+    __strMenu__(list);
+    printf("LENGTH: %d\n", lenMenu(list));
+}
+
+void formatTable(NodeTable* list) {
+
+    puts("---------------------------------");
+    __strTable__(list);
+    printf("LENGTH: %d\n", lenTable(list));
+}
+
+void testMenu(void) {
 
     Menu a = {"1", "a", 1, "none"}, b = {"2", "b", 2, "none"}, c = {"3", "c", 3, "none"};
 
-    Node* list = NULL;
+    NodeMenu* list = NULL;
 
-    list = push(0, list, __new__(&a));
-    format(list);
+    list = pushItem(0, list, __newItem__(&a));
+    formatItem(list);
 
-    list = pop(0, list);
-    format(list);
+    list = popItem(0, list);
+    formatItem(list);
 
-    list = pop(0, list);
+    list = popItem(0, list);
 
-    list = push(0, list, __new__(&a));
-    format(list);
+    list = pushItem(0, list, __newItem__(&a));
+    formatItem(list);
 
-    list = push(0, list, __new__(&b));
-    format(list);
+    list = pushItem(0, list, __newItem__(&b));
+    formatItem(list);
 
-    list = push(1, list, __new__(&c));
-    format(list);
+    list = pushItem(1, list, __newItem__(&c));
+    formatItem(list);
 
-    list = push(1, list, __new__(&b));
-    format(list);
+    list = pushItem(1, list, __newItem__(&b));
+    formatItem(list);
 
-    list = push(2, list, __new__(&a));
-    format(list);
+    list = pushItem(2, list, __newItem__(&a));
+    formatItem(list);
 
-    list = push(3, list, __new__(&b));
-    format(list);
+    list = pushItem(3, list, __newItem__(&b));
+    formatItem(list);
 
-    list = pop(0, list);
-    format(list);
+    list = popItem(0, list);
+    formatItem(list);
 
-    list = pop(0, list);
-    format(list);
+    list = popItem(0, list);
+    formatItem(list);
 
-    list = pop(1, list);
-    format(list);
+    list = popItem(1, list);
+    formatItem(list);
 
-    list = pop(2, list);
-    format(list);
+    list = popItem(2, list);
+    formatItem(list);
 
-    __del__(list);
+    __delMenu__(list);
+    return;
+}
+
+void testTable(void) {
+
+    Table a = {1,2,3}, b = {4,5,6}, c = {7,8,9};
+
+    NodeTable* list = NULL;
+
+    list = pushTable(0, list, __newTable__(&a));
+    formatTable(list);
+    writeTable(list);
+
+    list = pushTable(0, list, __newTable__(&b));
+    formatTable(list);
+    writeTable(list);
+
+
+
     return;
 }
