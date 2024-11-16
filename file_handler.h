89bf44c9a8661bfa8b9235d5c575a67c514169fa
AnishBramha <Anish.Teja@iiitb.ac.in>
2024-11-17 00:02:57 +0530

    Updated file_handler.h

--- file_handler.h
+++ file_handler.h
@@ -1,2 +1,9 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+#include "linked_list.h"
+
 int addMenuItem(char name[],float price,char allergens[],int type, int spice, int veg_nonveg );
 void printMenu();
+void writeTable(NodeTable* head);
+
+#endif
