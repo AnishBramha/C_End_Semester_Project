4f34c2b9ddbc2b55dbc4ebc6f3d02f0ca2f69a09
AnishBramha <Anish.Teja@iiitb.ac.in>
2024-11-17 19:33:33 +0530

    Updated customer.h

--- customer.h
+++ customer.h
@@ -1,1 +1,18 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
 
+#include "linked_list.h"
+#include "file_handler.h"
+
+Tables* reserveTable(long long int orderID, int people, Tables* tables, CurrentOrders* currentOrders, int* futureTableNo);
+// searches through Tables linked list, finds the first table with capacity >= people, sets 'available' to 0, returns head of Tables linked list,
+// sets table number to a particular order ID in CurrentOrders Linked List and modifies futureTableNo
+
+CurrentOrders* createOrder(char name[100], char phone[11], CurrentOrders* currentOrders, long int* orderID, Tables* tables);
+// creates a new node in the CurrentOrders linked list, with name, phone, creates and modifies an order ID, sets time and date, 
+// and pushes the new order to the CurrentOrders linked list and returns the head of the new list
+
+CurrentOrders* makeOrder(long long int itemID, CurrentOrders* currentOrders, long int orderID);
+// searches through the CurrentOrders linked list, finds the order ID, and updates the itemIDs[] array
+
+#endif
