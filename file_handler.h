6574085d5e3dbef4332a382fbf2593c7b37fbd1e
AnishBramha <Anish.Teja@iiitb.ac.in>
2024-11-17 19:35:17 +0530

    Updated file_handler.h

--- file_handler.h
+++ file_handler.h
@@ -1,9 +1,34 @@
 #ifndef FILE_HANDLER_H
 #define FILE_HANDLER_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include "linked_list.h"
 
-int addMenuItem(char name[],float price,char allergens[],int type, int spice, int veg_nonveg );
-void printMenu();
-void writeTable(NodeTable* head);
+#undef malloc
+void* alloc(size_t size);
+
+#undef fopen
+FILE* open(const char* fileName, const char* mode);
+
+Menu* loadMenu(void);
+void updateMenu(Menu* menu);
+// menu.csv format -> 
+
+Tables* loadTables(void);
+void updateTables(Tables*);
+// tables.csv format -> 
+
+CurrentOrders* loadCurrentOrders(void);
+void updateCurrentOrders(CurrentOrders* currentOrders);
+// current.csv format -> 
+
+OrderHistory* loadOrderHistory(void);
+void updateOrderHistory(OrderHistory* orderHistory);
+// history.csv format -> 
+
+void logAction(const char* fileName, const char* functionCalled);
+// actions.log format -> 
 
 #endif
