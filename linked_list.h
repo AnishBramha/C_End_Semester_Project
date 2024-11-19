52a76fe646ca7a7c3ee91f0a20cff2d8c639d2f0
AnishBramha <Anish.Teja@iiitb.ac.in>
2024-11-19 20:19:25 +0530

    Completed linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -91,8 +91,8 @@ int lenOrderHistory(OrderHistory* orderHistory);
 
 // ************************************************************
 
-char* getTime(void); // HH:MM (24-hr format)
-char* getDate(void); // DD:MM:YY
+char* getTime(void);
+char* getDate(void);
 
 // USE FOR DEBUGGING PURPOSES ONLY
 // ===============================
