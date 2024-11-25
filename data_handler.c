90c459fbdce3d7e2e97e277e05f013a9922fee6e
HarshaVPolar <hvr9849560299@gmail.com>
2024-11-25 21:10:35 +0530

    Update data_handler.c

--- data_handler.c
+++ data_handler.c
@@ -116,7 +116,7 @@ float totalRevenue(char* startDate, char* endDate, OrderHistory* orderHistory, i
   while(iter != NULL)
   {
     order = iter->order;
-    if(compdate(startDate,order.orderDate)==1||compdate(startDate,order.orderDate)==0){
+    if(compdate(order.orderDate,startDate)==-1||compdate(startDate,order.orderDate)==0){
       if(compdate(endDate,order.orderDate)==-1||compdate(endDate,order.orderDate)==0){
         rev += order.amount;
         (*totalOrders)++;
