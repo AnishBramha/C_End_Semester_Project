710a824c3e82a69fa69229ed76f28e85bedf8d11
HarshaVPolar <hvr9849560299@gmail.com>
2024-11-26 22:02:22 +0530

    Update data_handler.c

--- data_handler.c
+++ data_handler.c
@@ -1,21 +1,34 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #include "linked_list.h"
 #include "data_handler.h"
 
+
 int compdate(char* d1,char* d2);  //compares two dates and returns 0 if d1==d2, -1 if d1<d2 and 1 if d1>d2 when d1 and d2 are in format DD:MM:YY
 int diffdate(char* d1,char* d2); //compares two dates and return the number of days between d1 and d2 when d1 and d2 are in the format DD:MM:YY, only works when d2>d1
+void toLower(char* str);
 
 Menu* searchItemNames(char* name, Menu* menu){
   struct Menu* outlist = NULL;
   struct Menu* iter = menu;
   struct Item item;
+
+  if(strcmp(name," ")==0){
+    return menu;
+  }
+  toLower(name);
   while(iter != NULL)
   {
     item = iter->item;
-    char* check = strstr(item.name, name);
+    char tempstr[100];
+    strcpy(tempstr,item.name);
+    toLower(tempstr);
+
+    char* check = strstr(tempstr,name);
     if(check != NULL)
     {
       struct Menu* newlink = newItem(&item);
@@ -23,6 +36,13 @@ Menu* searchItemNames(char* name, Menu* menu){
     }
     iter = iter->next;
   }
+
+  if(outlist == NULL)
+  {
+    struct Item item = {0,"No such item found",0,""};  
+    struct Menu* newlink = newItem(&item);
+    outlist = addItem(newlink,outlist);
+  } 
   return outlist;
 }
 
@@ -41,6 +61,14 @@ Menu* searchVegEggNon(int code, Menu* menu){
     }
     iter = iter->next;
   }
+  
+  if(outlist == NULL)
+  {
+    struct Item item = {0,"No such item found",0,""};  
+    struct Menu* newlink = newItem(&item);
+    outlist = addItem(newlink,outlist);
+  }
+
   return outlist;
 }
 
@@ -59,6 +87,14 @@ Menu* searchCourse(int code, Menu* menu){
     }
     iter = iter->next;
   }
+  
+  if(outlist == NULL)
+  {
+    struct Item item = {0,"No such item found",0,""};  
+    struct Menu* newlink = newItem(&item);
+    outlist = addItem(newlink,outlist);
+  }
+
   return outlist;
 }
 
@@ -77,6 +113,14 @@ Menu* searchItemPrice(float min, float max, Menu* menu){
     }
     iter = iter->next;
   }
+  
+  if(outlist == NULL)
+  {
+    struct Item item = {0,"No such item found",0,""};  
+    struct Menu* newlink = newItem(&item);
+    outlist = addItem(newlink,outlist);
+  }
+
   return outlist;
 }
 
@@ -85,10 +129,15 @@ Menu* searchAllergens(char* allergen, Menu* menu){
   struct Menu* outlist = NULL;
   struct Menu* iter = menu;
   struct Item item;
+  toLower(allergen);
   while(iter != NULL)
   {
     item = iter->item;
-    char* flag = strstr(item.allergens,allergen);
+    char* tempstr[100];
+    strcpy(item.name,tempstr);
+    toLower(tempstr);
+
+    char* flag = strstr(tempstr,allergen);
     if(flag == NULL)
     {
       struct Menu* newlink = newItem(&item);
@@ -96,52 +145,50 @@ Menu* searchAllergens(char* allergen, Menu* menu){
     }
     iter = iter->next;
   }
+  
+  if(outlist == NULL)
+  {
+    struct Item item = {0,"No such item found",0,""};  
+    struct Menu* newlink = newItem(&item);
+    outlist = addItem(newlink,outlist);
+  }
+
   return outlist;
 }
 
 
-float totalRevenue(char* startDate, char* endDate, OrderHistory* orderHistory, int* totalOrders){
-  float rev = 0;
-  *totalOrders = 0;
-  struct OrderHistory* iter = orderHistory;
-  struct Order order;
-  if(strcmp(startDate,"")==0){
-    startDate = "00:00:00";
-  }
-  if(strcmp(endDate,"")==0){
-    endDate = "99:99:99";
-  }
+
+// SALES ANALYSIS
 
 
-  while(iter != NULL)
-  {
-    order = iter->order;
-    if(compdate(order.orderDate,startDate)==-1||compdate(startDate,order.orderDate)==0){
-      if(compdate(endDate,order.orderDate)==-1||compdate(endDate,order.orderDate)==0){
-        rev += order.amount;
-        (*totalOrders)++;
-    }}
-    iter = iter->next;
-  }
-  return rev;
+float totalRevenue(char* startDate, char* endDate, OrderHistory* orderHistory, int* totalOrders) {
+    float totalRevenue = 0.0;
+    *totalOrders = 0;
+
+    if (startDate == NULL || endDate == NULL || orderHistory == NULL) {
+        printf("Invalid input to totalRevenue function.\n");
+        return 0.0;
+    }
+
+    struct OrderHistory* iter = orderHistory;
+
+    while (iter != NULL) {
+        struct Order order = iter->order;
+
+        int compareStart = compdate(startDate, order.orderDate);
+        int compareEnd = compdate(order.orderDate, endDate);
+
+        if (compareStart <= 0 && compareEnd <= 0) {
+            totalRevenue += order.amount;
+            (*totalOrders)++;
+        }
+
+        iter = iter->next; 
+    }
+
+    return totalRevenue;
 }
 
-float averageRevenuePerDay(OrderHistory* orderHistory, int* totalOrders){
-  char currentdate[9] = "";
-  strcpy(currentdate, getDate());
-  char startdate[9] = "";
-  if(orderHistory==NULL)
-  {
-    return 0;
-  }
-  struct Order order;
-  order = orderHistory->order;
-  strcpy(startdate, orderHistory->order.orderDate);
-  int totdays = diffdate(startdate,currentdate) +1;
-  float rev = totalRevenue(startdate,currentdate,orderHistory,totalOrders);
-  rev = rev/totdays;
-  return rev;
-}
 
 float averageRevenueInLast30Days(OrderHistory* orderHistory, int* totalOrders ){
   float rev = 0;
@@ -167,66 +214,55 @@ float averageRevenueInLast30Days(OrderHistory* orderHistory, int* totalOrders ){
 
 void past30DaysData(OrderHistory* orderHistory, int data[30]){
   char currentdate[9]= "";
-  data = {0}
+ 
   strcpy(currentdate, getDate());
   struct OrderHistory* iter = orderHistory;
   struct Order order;
   
-  while(iter != NULL)
+  while(1==1)
   {
+    
     order = iter->order;
     int daydiff = diffdate(order.orderDate,currentdate);
     int i = 29 - daydiff;
     if(daydiff<30 && daydiff>=0){
      data[i] += order.amount;
     }
+    if(iter->next==NULL){
+      break;
+    }
     iter = iter->next;
   }
+
+  
+
+
 }
 
-int compdate(char* d1,char* d2){
-  int flag = -2;
-  if((int)d1[6]>(int)d2[6])
-  {
-    flag = -1;
-  }
-  else if((int)d1[7]>(int)d2[7])
-  {
-    flag = -1;
-  }
-  else if((int)d1[3]>(int)d2[3])
-  {
-    flag = -1;
-  }
-  else if((int)d1[4]>(int)d2[4])
-  {
-    flag = -1;
-  }
-  else if((int)d1[0]>(int)d2[0])
-  {
-    flag = -1;
-  }
-  else if((int)d1[1]>(int)d2[1])
-  {
-    flag = -1;
-  }
-  else if(strcmp(d1,d2)==0)
-  {
-    flag = 0;
-  }
-  else
-  {
-    flag = 1;
-  }   
-  return flag;
+
+int compdate( char* d1,  char* d2) {
+    struct tm tm1 = {0}, tm2 = {0};
+    sscanf(d1, "%02d-%02d-%02d", &tm1.tm_mday, &tm1.tm_mon, &tm1.tm_year);
+    sscanf(d2, "%02d-%02d-%02d", &tm2.tm_mday, &tm2.tm_mon, &tm2.tm_year);
+
+    tm1.tm_mon -= 1;
+    tm2.tm_mon -= 1;
+    tm1.tm_year += 100;
+    tm2.tm_year += 100;
+
+    time_t time1 = mktime(&tm1);
+    time_t time2 = mktime(&tm2);
+
+    return (time1 < time2) ? -1 : (time1 > time2 ? 1 : 0);
 }
+
 
 int diffdate(char *d1,char *d2) {
     struct tm tm1 = {0}, tm2 = {0};
     time_t time1, time2;
     
-    sscanf(d1, "%d:%d:%d", &tm1.tm_mday, &tm1.tm_mon, &tm1.tm_year);
-    sscanf(d2, "%d:%d:%d", &tm2.tm_mday, &tm2.tm_mon, &tm2.tm_year);
+    sscanf(d1, "%02d-%02d-%02d", &tm1.tm_mday, &tm1.tm_mon, &tm1.tm_year);
+    sscanf(d2, "%02d-%02d-%02d", &tm2.tm_mday, &tm2.tm_mon, &tm2.tm_year);
     
     tm1.tm_mon -= 1;  
     tm2.tm_mon -= 1;
@@ -241,3 +277,37 @@ int diffdate(char *d1,char *d2) {
     return (int)difference;
 }
 
+
+
+
+
+
+float todaysSalesData(OrderHistory* orderHistory, int* totalOrders) {
+    char currentdate[9] = "";
+    strcpy(currentdate, getDate());  
+    
+    float totalRevenue = 0.0;
+    *totalOrders = 0;
+
+    struct OrderHistory* iter = orderHistory;
+
+    while (iter != NULL) {
+        struct Order order = iter->order;
+
+        
+        if (strcmp(order.orderDate, currentdate) == 0) {
+            totalRevenue += order.amount;
+            (*totalOrders)++;
+        }
+
+        iter = iter->next; 
+    }
+
+    return totalRevenue;
+}
+
+void toLower(char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower((unsigned char)str[i]);
+    }
+}
