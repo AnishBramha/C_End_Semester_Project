4ce807707b652708dc825889d886eb7c7f5e1c9f
Aryan-Sharma05 <aryan.sharma@iiitb.ac.in>
2024-11-28 02:53:00 +0530

    Beta 1.5

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+int OS=2;
 
+#include "ui.h"
+#include "upi_qr.c"
+
+
+
+int main(void) {
+    Menu *menuHead = loadMenu();
+    Tables *tablesHead = loadTables();
+    OrderHistory *orderHistoryHead = loadOrderHistory();
+    CurrentOrders *currentOrdersHead = loadCurrentOrders();
+
+    // OsSelectionPage(PASSHEADS);
+    // SalesAnalysisPage(PASSHEADS);
+    StartupPage(PASSHEADS);
+
+
+    deleteMenu(menuHead);
+    deleteTables(tablesHead);
+    deleteOrderHistory(orderHistoryHead);
+    deleteCurrentOrders(currentOrdersHead);
+
+    return 0;
+}
