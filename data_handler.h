e92fd9543e9df7e8deec8e2c891545662ceadce9
Aryan-Sharma05 <aryan.sharma@iiitb.ac.in>
2024-11-28 02:51:04 +0530

    Update data_handler.h

--- data_handler.h
+++ data_handler.h
@@ -1,9 +1,10 @@
-#ifndef DATA_HANDLER_H
-#define DATA_HANDLER_H
+// #ifndef DATA_HANDLER_H
+// #define DATA_HANDLER_H
 
 #include "linked_list.h"
+#include <string.h>
 
-Menu* searchItemName(char* name, Menu* menu); // returns pointer to Menu struct with item details
+Menu* searchItemNames(char* name, Menu* menu); // returns pointer to Menu struct with item details
 Menu* searchVegEggNon(int code, Menu* menu);                // these three functions return pointer to head of 
 Menu* searchCourse(int code, Menu* menu); 
 Menu* searchItemPrice(float min, float max, Menu* menu);    // a new linked list, which is made after
@@ -13,5 +14,5 @@ float totalRevenue(char* startDate, char* endDate, OrderHistory* orderHistory, i
 float averageRevenuePerDay(OrderHistory* orderHistory, int* totalOrders); // Returns today's sales data along with total Orders made today
 float averageRevenueInLast30Days(OrderHistory* orderHistory, int* totalOrders ); // returns past 1 month sales data along with total Orders made in 30 days
 void past30DaysData(OrderHistory* orderHistory, int data[30]); // makes changes in data array provided. (as it will also get reflect back to the caller). And return the total revenue
-
-#endif
+float todaysSalesData(OrderHistory* orderHistory, int* totalOrders) ;
+// #endif
